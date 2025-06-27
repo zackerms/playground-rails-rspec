0813b83b3a4dd85ff7c937bdc953df3f6b5c1b7c
Masaki Ishizaka <masaki.ishizaka@giftee.co>
2025-06-27 17:24:53 +0900

    feat: indexページのテストを追加

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :controller do
   controller do
+    def index
+      render plain: "Hello, World!"
+    end
+
     def not_found
       render_404
+    end
+  end
+
+  describe 'GET #index' do
+    it 'renders the index page with correct status' do
+      get :index
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("Hello, World!")
     end
   end
 
